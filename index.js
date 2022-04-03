4799ea9e6e30ebcfb17323d3570bf7d9a0841def
Sashwat-43 <mishrashashwat43@gmail.com>
2022-04-03 13:30:29 +0530

    second commit

--- index.js
+++ index.js
@@ -9,21 +9,18 @@ dotenv.config();
 
 // console.log(process.env);
 
-const y= process.env.x;
-console.log(y);
+// const y= process.env.x;
+// console.log(y);
 
-// mongoose.connect(
-//     // "mongodb+srv://SocialMedia:SocialMedia123@mysocial.l5o4f.mongodb.net/SocialData?retryWrites=true&w=majority",
-//     process.env.MONGO_URI,
-//     { useNewUrlParser: true, useUnifiedTopology: true },
-//     ()=>{
-//         console.log("Connected to Mongo Db");
-//     }
-// );
+mongoose.connect(
+    process.env.MONGO_URL,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    ()=>{
+        console.log("Connected to Mongo Db");
+    }
+);
 
 app.listen(8800,()=>{
     console.log("Server is running!")
-    console.log(process.env);
-    // console.log(process.env.MONGO_URI);
-    // console.log("mongodb+srv://SocialMedia:SocialMedia123@mysocial.l5o4f.mongodb.net/SocialData?retryWrites=true&w=majority");
+
 })
