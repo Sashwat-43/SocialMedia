bf455ed36acfc0c074fe8b2eef15254596e31297
Sashwat-43 <mishrashashwat43@gmail.com>
2022-04-04 16:41:43 +0530

    added delete functionality for user

--- routes/users.js
+++ routes/users.js
@@ -8,35 +8,66 @@ const User = require('../models/User');
 // updating the user's info
 
 router.put("/:id",async(req,res)=>{
+
     console.log(req.params.id);
     console.log(req.body.userId);
+    
+    // if user id matches with id passed in url or the user is admin itself
+
     if(req.params.id==req.body.userId||req.body.isAdmin){
 
+        // if we have to update the password also, then we will have to encrypt it using bcrypt and then
+        // store the encrypted password in my db
+
         if(req.body.password){
-            console.log(req.body.password);
+            // console.log(req.body.password);
             try{
-                const salt = await bcrypt.genSalt(10);
+                const saltRounds = 10;
+                const salt = await bcrypt.genSalt(saltRounds);
                 req.body.password = await bcrypt.hash(req.body.password,salt);
-                res.status(200).json("User has been updated successfully!");
             }catch(err){
                 return res.status(404).json(err);
             }
         }
+
+        // if we have to update data other than password then we don't have to encrypt it
+        // we'll store it normally without any encryption
+
         try{
-            const user = await User.findByIdAndUpdate(req.params.id,{
+            const updatedUser = await User.findByIdAndUpdate(req.body.userId,{
                 $set:req.body,
             });
-            return res.status(200).json("User has been updated successfully!");
+            res.status(200).json("User has been updated successfully!");
         }catch(err){
-            return res.status(500).json(err);
+            return res.status(404).json(err);
         }
 
+        // else the user is not authorised to change/update any other user's data
+
     }else{
-        return res.status(403).json("Not authorised to update other's account!");
+        return res.status(401).json("Not authorised to update other's account!");
     }
 })
 
 // deleting a particular user
+
+router.delete("/:id",async(req,res)=>{
+
+    if(req.body.userId==req.params.id||req.body.isAdmin){
+
+        try{
+            const deletedUser = await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("User has been deleted!");
+        }catch(err){
+            return res.status(404).json(err);
+        }
+
+    }else{
+        return res.status(401).json("Not authorised to delete other's account!");
+    }
+
+})
+
 
 // reading/getting a particular user
 
