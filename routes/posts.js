d4559c2841d196eb8a69366d343f1e3382178d02
Sashwat-43 <mishrashashwat43@gmail.com>
2022-04-05 15:00:18 +0530

    added unlike post functionality

--- routes/posts.js
+++ routes/posts.js
@@ -96,8 +96,53 @@ router.delete("/:id",async(req,res)=>{
 
 // liking a post
 
-router.put("/:id/like",async(req,res)=>{
+// first of all we find whether post exists with this params.id or not
+// if not then we restrict this action and prompt that no such post exists
+// else now we check whether the user has already liked this post or not
+// if yes then we restrict this action and prompt that you have already liked this post
+// else we insert the user id in likes array of post with params.id
 
+
+router.put("/:id/like",async(req,res)=>{
+    try{
+        const tempPost = await Post.findById(req.params.id);
+        if(!tempPost){
+            return res.status(501).json("No such post exists!");
+        }
+        if (tempPost.likes.includes(req.body.userId)){
+            return res.status(501).json("You have already liked this post!");
+        }else{
+            await tempPost.updateOne({$push:{likes:req.body.userId}});
+            res.status(200).json("Post liked successfully!");
+        }
+    }catch(err){
+        res.status(404).json(err);
+    }
+})
+
+// unliking a post
+
+// first of all we find whether post exists with this params.id or not
+// if not then we restrict this action and prompt that no such post exists
+// else now we check whether the user has liked this post or not
+// if not then we restrict this action and prompt that you have not liked this post
+// else we remove the user id from likes array of post with params.id
+
+router.put("/:id/unlike",async(req,res)=>{
+    try{
+        const tempPost = await Post.findById(req.params.id);
+        if(!tempPost){
+            return res.status(501).json("No such post exists!");
+        }
+        if (!tempPost.likes.includes(req.body.userId)){
+            return res.status(501).json("You have not liked this post, so cannot unlike the post!");
+        }else{
+            await tempPost.updateOne({$pull:{likes:req.body.userId}});
+            res.status(200).json("Post unliked successfully!");
+        }
+    }catch(err){
+        res.status(404).json(err);
+    }
 })
 
 // getting all posts of user's followings
