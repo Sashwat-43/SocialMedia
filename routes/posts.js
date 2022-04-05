29aea5ad557aa157ff903fdda04f455988c84377
Sashwat-43 <mishrashashwat43@gmail.com>
2022-04-05 14:47:35 +0530

    added functionality if no post exists

--- routes/posts.js
+++ routes/posts.js
@@ -31,6 +31,9 @@ router.put("/:id",async(req,res)=>{
 
     try{
         const tempPost = await Post.findById(req.params.id);
+        if(!tempPost){
+            return res.status(404).json("No such post exists!");
+        }
         if(tempPost.userId == req.body.userId){
             await tempPost.updateOne({$set:req.body});
             res.status(200).json("Post updated successfully!");
@@ -46,9 +49,17 @@ router.put("/:id",async(req,res)=>{
 
 // getting a post
 
+// first of all we check whether there exists post with id as params.id or not
+// if not then we restrict this action and prompt that no such post exists
+// else we get the post by findById
+
 router.get("/:id",async(req,res)=>{
     try{
         const tempPost = await Post.findById(req.params.id);
+        if(!tempPost)
+        {
+            return res.status(404).json("No such post exists!");
+        }
         const {userId,_id,bio,likes,...others} = tempPost;
         res.status(200).json({userId,_id,bio,likes});
     }catch(err){    
@@ -58,7 +69,36 @@ router.get("/:id",async(req,res)=>{
 
 // deleting a post
 
+
+// first of all we check whether there exists post with id as params.id or not
+// if not then we restrict this action and prompt that no such post exists
+// else
+// for deleting we firstly check whether the post is created by user with body.userId or not
+// if not then we restrict this action and prompt that you have not created this post so you cannot delete this post
+// else we simply find the post by params.id and delete this post
+
+router.delete("/:id",async(req,res)=>{
+    try{
+        const tempPost = await Post.findById(req.params.id);
+        if(!tempPost){
+            return res.status(404).json("No such post exists!");
+        }
+        if(tempPost.userId==req.body.userId){
+            await tempPost.deleteOne();
+            res.status(200).json("Post deleted successfully!");
+        }else{
+            res.status(501).json("You have not created this post so you cannot delete this post!");
+        }
+    }catch(err){
+        res.status(404).json(err);
+    }
+})
+
 // liking a post
+
+router.put("/:id/like",async(req,res)=>{
+
+})
 
 // getting all posts of user's followings
 
