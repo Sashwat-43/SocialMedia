1dad780929f8447aaa3b8b19187cc5c361815b56
Sashwat-43 <mishrashashwat43@gmail.com>
2022-04-05 12:57:39 +0530

    added update post functionality

--- routes/posts.js
+++ routes/posts.js
@@ -21,6 +21,29 @@ router.post("/",async(req,res)=>{
 
 // updating a post
 
+// for updating firstly we find the post with id of params.id
+// now we check whether this post is created by the user with userId or not
+// if no then we restrict this action and prompt that you have not created this post, so you cannot update this post
+// else we update this post
+
+
+router.put("/:id",async(req,res)=>{
+
+    try{
+        const tempPost = await Post.findById(req.params.id);
+        if(tempPost.userId == req.body.userId){
+            await tempPost.updateOne({$set:req.body});
+            res.status(200).json("Post updated successfully!");
+        }else{
+            res.status(501).json("You have not created this post so you cannot update this post!")
+        }
+    }catch(err){
+        res.status(404).json(err);
+    }
+    
+
+})
+
 // getting a post
 
 // deleting a post
